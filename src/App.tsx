f9bfceb1dff0873a713f5d4eb688cc82add903fa
Quoc Hoang <hoangvanquoc923@gmail.com>
2025-02-14 14:59:01 +0700

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+import "./index.css"
+function App() {
+
+  return (
+    <>
+      <div className="bg-blue">
+        <p className="text-danger">This is a sentence</p>
+      </div>
+    </>
+  )
+}
+
+export default App
