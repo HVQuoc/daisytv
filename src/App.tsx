74f180c283df53569e46c02ae23d6ca65083f6dd
Quoc Hoang <hoangvanquoc923@gmail.com>
2025-02-14 15:39:47 +0700

    define layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,22 @@
-import './App.css'
-import "./index.css"
+import { Routes, Route } from "react-router-dom";
+import "./index.css";
+import Home from "./pages/Home";
+import Search from "./pages/Search";
+import Details from "./pages/Details";
+import Explore from "./pages/Explore";
+import MainLayout from "./Layout/MainLayout";
 function App() {
-
   return (
-    <>
-      <div className="bg-blue">
-        <p className="text-danger">This is a sentence</p>
-      </div>
-    </>
-  )
+    <Routes>
+      <Route element={<MainLayout />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/search" element={<Search />} />
+        <Route path="/:explore" element={<Explore />}>
+          <Route path=":id" element={<Details />} />
+        </Route>
+      </Route>
+    </Routes>
+  );
 }
 
-export default App
+export default App;
